dee91f2cc080788ab14d626dfdb668b6cbe1da95
pavel.strobl@prusa3d.cz <pavel.strobl@prusa3d.cz>
2025-10-10 11:52:05 +0200

    Start scripts

--- start_docker.sh
+++ start_docker.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# Check the operating system
 if [[ "$(uname)" == "Darwin" ]]; then
-    # macOS-specific command
     INTERFACES=$(ifconfig)
     export HOST_IP=$(echo "$INTERFACES" | grep 'inet ' | grep -v '127.0.0.1' | grep -v 'docker' | grep -v 'veth' | awk '$2 !~ /^172\./ {print $2}')
 else
-    # Linux-specific command (your original script)
     INTERFACES=$(ip a)
     export HOST_IP=$(echo "$INTERFACES" | grep 'inet ' | grep -v '127.0.0.1' | grep -v 'docker' | grep -v 'veth' | grep -v 'br-' | awk '{print $2}' | cut -d'/' -f1)
 fi
 
-# Check if an IP was found
 if [ -z "$HOST_IP" ]; then
     echo "Could not find a valid host IP address."
     exit 1
@@ -19,5 +15,4 @@ fi
 
 echo "Using host IP: $HOST_IP"
 
-# Use the retrieved IP in your docker compose command
 docker compose up
